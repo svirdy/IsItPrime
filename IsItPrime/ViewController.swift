fe5e86c4376ffad756e054914e1cad60d473c069
Sachin Virdy <svirdy1@gmail.com>
2015-10-24 15:21:17 -0700

    auto layout and basic functionality

--- IsItPrime/ViewController.swift
+++ IsItPrime/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var number: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var numberInt = number.text.toInt()
+        
+        if numberInt != nil {
+            
+            var unwrappedNumber = numberInt!
+            var isPrime = true
+            
+            if unwrappedNumber == 1 {
+                isPrime = false
+            }
+            
+            if unwrappedNumber != 2 && unwrappedNumber != 1 {
+                
+                for var i = 2; i < unwrappedNumber; i++ {
+                    if unwrappedNumber % i == 0 {
+                        isPrime = false
+                    }
+                }
+            }
+            
+            if isPrime == true {
+                resultLabel.text = "\(unwrappedNumber) is prime!"
+            } else {
+                resultLabel.text = "\(unwrappedNumber) is not prime!"
+            }
+            
+        } else {
+            resultLabel.text = "Please enter a number"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
